eed574f376e330005401e539c9d5ab1cfc52b5df
Armaan-Hossain-Niloy <niloy.barca@outlook.com>
2023-10-21 08:31:23 +0600

    add to cart section add successfully

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,11 +1,15 @@
 import { useLoaderData } from "react-router-dom";
+import CartDetails from "./CartDetails";
 
 
 const Cart = () => {
     const cartData = useLoaderData();
+    
     return (
-        <div>
-            
+        <div className="grid grid-cols-2 gap-5 mx-10">
+            {
+                cartData.map((cart, idx) => <CartDetails key={idx} cart={cart}></CartDetails>)
+            }
         </div>
     );
 };
